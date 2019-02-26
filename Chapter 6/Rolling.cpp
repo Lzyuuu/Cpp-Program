7447f7e43f5ff04c73ed89acd9a688fd863b2aad
Lzyuuu <280561636@qq.com>
2019-02-26 10:48:37 +0800

    Update Rolling.cpp

--- Chapter 6/Rolling.cpp
+++ Chapter 6/Rolling.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 int main() {
+    // initialize six face frequency
+    unsigned int frequency1{0};
+    unsigned int frequency2{0};
+    unsigned int frequency3{0};
+    unsigned int frequency4{0};
+    unsigned int frequency5{0};
+    unsigned int frequency6{0};
+    int face{0};
     
+    for (unsigned int roll{1}; roll <= 60000000; ++roll) {
+        face = 1 + rand()
+    }
 }
